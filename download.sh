e265f21c3c4ebee1bb0d4236ca576f67cfb1989e
amwheeler1 <62343862+amwheeler1@users.noreply.github.com>
2021-02-16 09:32:23 -0500

    Create download.sh
    A shell script that can be ran in a terminal window to copy this repository to a computer. If it won't run on your system, type

---
+++ download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script clones the current state of the CISP1020 repository into a local directory
+
+
+# If this script does not work on your system, enter the following command to make the
+# script executable:
+# chmod +x download.sh
+
+echo "Cloning Repository Into Directory 'CISP1020' "
+git clone https://github.com/amwheeler1/CISP1020
+echo "Cloning Complete!"
