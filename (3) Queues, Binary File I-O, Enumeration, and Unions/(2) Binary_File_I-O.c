2351cdec5acfd3a76c90a92088117de941ca1bb2
amwheeler1 <62343862+amwheeler1@users.noreply.github.com>
2021-02-18 07:17:29 -0500

    Update (2) Binary_File_I-O.c
    Updated to have better examples, fix some minor issues, and include explanation of fseek()

--- (3) Queues, Binary File I-O, Enumeration, and Unions/(2) Binary_File_I-O.c
+++ (3) Queues, Binary File I-O, Enumeration, and Unions/(2) Binary_File_I-O.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 typedef struct employee {
-	char firstName [25], lastName [25];
+	char* firstName,* lastName;
 	int age;
 	double salary;
 } Employee;
@@ -19,9 +19,15 @@ int main () {
 
 	Employee eList [100];
 
-	/*
-	 * Assume eList has been initialized to a set list of Employees
-	*/
+    eList[0].firstName = "Bob";
+    eList[0].lastName  = "Smitty";
+    eList[0].age = 50;
+    eList[0].salary = 63422.96;
+
+    eList[1].firstName = "Jimmy";
+    eList[1].lastName  = "BillyBob";
+    eList[1].age = 36;
+    eList[1].salary = 46589.24;
 
 	// Accessing a file as binary is typically faster than using an encoding scheme, but the downfall comes with
 	// having to know exactly how the file is structured.
@@ -41,10 +47,46 @@ int main () {
 	// is used instead
 	binRead = fopen("data.bin", "rb");
 
+    // If an error has occurred in the file stream, ferror() can be used to determine this
+    // Syntax: ferror(FILE*)
+    if (ferror(binRead)) {
+        printf("\nAn error has occurred with the input file stream.\n\n");
+        return 1;
+    }
+
 	// fread uses a very similar syntax to fwrite, except an input file stream is used instead of an
 	// output file stream	
-	for (int i = 0; i < 100; i++)
-		fread(&eList[i], sizeof(Employee), 1, binRead);
+	for (int i = 0; i < 100; i++) {
+		fread(&systemUsers[i], sizeof(Employee), 1, binRead);
+
+        // feof() can be used to determine if the end of the file stream has been reached for FILE*
+        // Syntax: feof(FILE*)
+        if (feof(binRead))
+            break;
+
+        // ftell() can be used to determine which byte in the file the pointer is currently at
+        // Syntax: ftell(FILE*)
+        printf("\nCurrent Byte: %ld", ftell(binRead));
+    }
+    printf("\n\n");
+
+    for (int i = 0; i < 2; i++) {
+        printf("\nEmployee Name : %s %s", systemUsers[i].firstName, systemUsers[i].lastName);
+        printf("\nEmployee  Age : %d"   , systemUsers[i].age);
+        printf("\nCurrent Salary: $%.2lf"  , systemUsers[i].salary);
+    }
+
+    // fseek() can be used to set the byte position of a file pointer
+    // Syntax: fseek(FILE*, offset, start@Byte)
+    fseek(binRead, 0, 0);   // Puts FILE* at beginning of file
+    printf("\n\nCurrent Byte Position is %ld\n", ftell(binRead));
+    // fseek also has a number of constants associated with it that specify unique positions
+    // SEEK_SET indicates the beginning of the file
+    // SEEK_CUR indicates the current position of FILE*
+    // SEEK_END indicates the end of the file
+    fseek(binRead, 0, SEEK_END);
+    printf("\nCurrent Byte Position is %ld\n\n", ftell(binRead));
+
 
 	// An important thing to note about reading and writing in binary is that structures are not
 	// the same size as the sum of their parts. Data types must be read in in the same manner they
